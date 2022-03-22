a323f3c07bb49657e923602d7acc5b036f68dfaa
kmm2003 <69188747+kmm2003@users.noreply.github.com>
2022-03-22 17:57:36 +0900

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,31 @@
+import os
 from flask import *
 
-app = Flask(__name__)
+PROJECT_ROOT=os.path.dirname(os.path.abspath(__file__))
+TEMPLATES=os.path.join(PROJECT_ROOT,'templates')
+STATIC_FOLDER=os.path.join(PROJECT_ROOT,'static')
+app = Flask(__name__,
+            template_folder=TEMPLATES,
+            static_folder=STATIC_FOLDER)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/controller/')
+def controller():
+    return render_template('controller.html')
+
+@app.route('/api/command/', methods=['POST'])
+def command():
+    cmd=request.form.get('command')
+    return jsonify(status='success'), 200
+
+'''
+@app.route('/setting/')
+def controller():
+    return render_template('setting.html')
+'''
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port="9999")
+    app.run(host='0.0.0.0',port="9999", threaded=True)
