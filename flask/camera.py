f02a1b9f3747ea4d4ce3e9cefde412c0443be941
KooSeongMin <37334007+17011607@users.noreply.github.com>
2022-03-30 22:49:30 +0900

    Update camera.py

--- flask/camera.py
+++ flask/camera.py
@@ -7,7 +7,7 @@ class camera:
         self.PORT = PORT
         self.sock.bind(('0.0.0.0', 9998))
     def send(self, data):
-        self.sock.sendto(data.encode(), (self.HOST, self.PORT));
+        self.sock.sendto(data.encode(), (self.HOST, self.PORT)); # Camera Commnad are only SS, LS, RS, LU, LD, RU, RD
 
 
 cam = camera('192.168.137.47', 4210)
