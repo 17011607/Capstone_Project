8449a75fdc8bc4e372650cbda969db2f24a5061f
KooSeongMin <37334007+17011607@users.noreply.github.com>
2022-03-30 22:46:43 +0900

    Add files via upload

---
+++ flask/camera.py
@@ -0,0 +1,15 @@
+import socket
+
+class camera:
+    def __init__(self, HOST, PORT):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.HOST = HOST
+        self.PORT = PORT
+        self.sock.bind(('0.0.0.0', 9998))
+    def send(self, data):
+        self.sock.sendto(data.encode(), (self.HOST, self.PORT));
+
+
+cam = camera('192.168.137.47', 4210)
+value = input("value :")
+cam.send(value)
