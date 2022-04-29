021d404119180673c2e163bb1630afba181a77a2
kmm2003 <69188747+kmm2003@users.noreply.github.com>
2022-04-29 15:16:22 +0900

    Add files via upload

---
+++ flask/static/js/file_upload.js
@@ -0,0 +1,66 @@
+var fileList = []; //파일 정보를 담아 둘 배열
+$(function(){
+
+    //드래그앤드랍
+    $("#fileDrop").on("dragenter", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+    }).on("dragover", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).css("background-color", "#FFD8D8");
+    }).on("dragleave", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).css("background-color", "#FFF");
+    }).on("drop", function(e){
+        e.preventDefault();
+
+        var files = e.originalEvent.dataTransfer.files;
+        if(files != null && files != undefined){
+            var tag = "";
+            for(i=0; i<files.length; i++){
+                var f = files[i];
+                fileList.push(f);
+                var fileName = f.name;
+                var fileSize = f.size / 1024 / 1024;
+                fileSize = fileSize < 1 ? fileSize.toFixed(3) : fileSize.toFixed(1);
+                tag += 
+                        "<div class='fileList'>" +
+                            "<span class='fileName'>"+fileName+"</span>" +
+                            "<span class='fileSize'>"+fileSize+" MB</span>" +
+                            "<span class='clear'></span>" +
+                        "</div>";
+            }
+            $(this).append(tag);
+        }
+
+        $(this).css("background-color", "#FFF");
+    });
+
+    //저장
+    $(document).on("click", "#save", function(){
+        var formData = new FormData($("#fileForm")[0]);
+        if(fileList.length > 0){
+            fileList.forEach(function(f){
+                formData.append("fileList", f);
+            });
+        }         
+
+        $.ajax({
+            url : "upload",
+            data : formData,
+            type:'POST',
+            enctype:'multipart/form-data',
+            processData:false,
+            contentType:false,
+            dataType:'json',
+            cache:false,
+            success:function(res){
+                alert("저장에 성공하셨습니다.");
+            },error:function(res){
+                alert("오류 발생.\n관리자에게 문의해주세요.");
+            }
+        });
+    });
+});
