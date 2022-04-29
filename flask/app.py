d07f95c212587e72907f9853e84b266e3e8c5179
KooSeongMin <37334007+17011607@users.noreply.github.com>
2022-04-29 14:59:21 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -15,6 +15,12 @@ app = Flask(__name__,
 def get_drone():
     return DroneManager()
 
+def video_generator():
+    drone = get_drone()
+    for jpeg in drone.video_jpeg_generator():
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r\n')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -34,13 +40,18 @@ def command():
         direction = request.form.get('direction')
         print(f"direction : {direction}")
         if direction.find('R') != -1 :
-            drone.right()
+            print("R")
+            drone.right(100)
         if direction.find('L') != -1 :
-            drone.left()
+            print("L")
+            drone.left(100)
         if direction.find('U') != -1 :
-            drone.forward()
+            print("U")
+            drone.forward(100)
         if direction.find('D') != -1 :
-            drone.back()
+            print("D")
+            drone.back(100)
+            
 
     elif cmd == "takeoff":
         drone.takeoff()
@@ -66,18 +77,24 @@ def get_credentials():
     make_token()
     return render_template('setting.html')
 
-
-
-def video_generator():
+@app.route('/setting/ap')
+def video_feed():
     drone = get_drone()
-    for jpeg in drone.video_jpeg_generator():
-        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r\n')
+    ssid = request.form.get('SSID')
+    password = request.form.get('password')
+    drone.send_command(f'ap {ssid} {password}')
+    return render_template('setting.html')
 
 
 @app.route('/video/streaming')
 def video_feed():
     return Response(video_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
+
+
+
 if __name__ == '__main__':
     #cam = camera('192.168.137.47', 4210)
+    #temp_direction = "SS"
     app.run(host='0.0.0.0',port="9999", threaded=True)
