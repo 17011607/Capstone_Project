8a586e03f73f983038bd53e802c5ae3259e11e86
KooSeongMin <37334007+17011607@users.noreply.github.com>
2022-05-03 21:20:26 +0900

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,8 @@ from camera import camera
 from drone_manager import DroneManager
 from werkzeug.utils import secure_filename
 from googleDrive import *
+import time
+import threading
 
 PROJECT_ROOT=os.path.dirname(os.path.abspath(__file__))
 TEMPLATES=os.path.join(PROJECT_ROOT,'templates')
@@ -15,16 +17,47 @@ app = Flask(__name__,
 def get_drone():
     return DroneManager()
 
+def video_generator():
+    drone = get_drone()
+    for jpeg in drone.video_jpeg_generator():
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r\n')
+
+def move_control():
+    global status
+    drone = get_drone
+    while 1:
+        if status == 'R' :
+            #drone.right(20)
+            print("right")
+        elif status == 'L' :
+            #drone.left(20)
+            print("left")
+        elif status == 'U' :
+            #drone.forward(20)
+            print("forward")
+        elif status == 'D':
+            #drone.back(20)
+            print("down")
+        elif status == 'LU':
+            #drone.send_command(f"go 30 30 0 100")
+            print("Left up")
+        elif status == 'RU' :
+            #drone.send_command(f"go -30 30 0 100")
+            print("Right up")
+        elif status == 'LD':
+            #drone.send_command(f"go 30 -30 0 100")
+            print("left down")
+        elif status == 'RD':
+            #drone.send_command(f"go -30 -30 0 100")
+            print("right down")
+        else:
+            #drone.send_command(f"go 0 0 0 10")
+            print("stop")
+        time.sleep(1)
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# https://kr.freepik.com/premium-vector/battery-icons-charge-level-ui-design-elements-of-battery-full-low-and-empty-battery-status-set-of-smartphone-battery-charge-level-indicators_17743907.htm
-@app.route('/battery')
-def drone_battery():
-    drone=get_drone()
-    battery=drone.battery()
-    return  jsonify({"battery":battery})
 
 @app.route('/controller/')
 def controller():
@@ -34,33 +67,16 @@ def controller():
 def command():
     drone = get_drone()
     cmd=request.form.get('command')
-    if cmd=="cammove":
+    if cmd == "cammove":
         direction = request.form.get('direction')
-        print(f"direction : {direction}")
-        if direction.find('R') != -1 :
-            drone.clockwise(5)
-        if direction.find('L') != -1 :
-            drone.counter_clockwise(5)
-        if direction.find('U') != -1 :
-            drone.up(20)
-        if direction.find('D') != -1 :
-            drone.down(20)
-
+        #cam.send(direction)
     elif cmd == "dronemove":
+        global status
         direction = request.form.get('direction')
         print(f"direction : {direction}")
-        x = 0
-        y = 0
-        if direction.find('R') != -1 :
-            x -= 30
-        if direction.find('L') != -1 :
-            x += 30
-        if direction.find('U') != -1 :
-            y += 30
-        if direction.find('D') != -1 :
-            y -= 30
-        drone.send_command(f"go {x} {y} 0 100 blocking=False")
-
+        status = direction
+        
+    
     elif cmd == "takeoff":
         drone.takeoff()
 
@@ -69,10 +85,6 @@ def command():
 
     elif cmd == "streamon":
         drone.streamon()
-    
-    elif cmd == "speed":
-        speed=request.form.get('speed')
-        drone.set_speed(speed)
 
     return jsonify(status='success'), 200
 
@@ -97,21 +109,19 @@ def ap():
     drone.send_command(f'ap {ssid} {password}')
     return render_template('setting.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
-
-
-def video_generator():
-    drone = get_drone()
-    for jpeg in drone.video_jpeg_generator():
-        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r\n')
-
 
 @app.route('/video/streaming')
 def video_feed():
     return Response(video_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
+
+
+
 if __name__ == '__main__':
     #cam = camera('192.168.137.47', 4210)
+    #temp_direction = "SS"
+    status = "S"
+    _move = threading.Thread(target=move_control)
+    _move.start()
     app.run(host='0.0.0.0',port="9999", threaded=True)
