e406051e8c7191e973ddef3540dfa5e982ca5f50
KooSeongMin <37334007+17011607@users.noreply.github.com>
2022-05-10 14:08:23 +0900

    Add files via upload

--- flask/drone_state.py
+++ flask/drone_state.py
@@ -3,18 +3,13 @@ from time import sleep
 
 
 class drone_status():
-    local_ip = ''
-    local_port = 8890 ## read port
-    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
-    socket.bind((local_ip, local_port))
-
-    def __init__(self):
-        self.local_ip = ''
-        self.local_port = 8890
+    def __init__(self, drone_ip = '192.168.10.1', host_ip = '192.168.10.2'):
+        self.host_ip = host_ip
+        self.host_port = 8890
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        socket.bind((self.local_ip, self.local_port))
+        self.socket.bind((self.host_ip, self.host_port))
 
-        self.tello_ip = '192.168.10.1'
+        self.tello_ip = drone_ip
         self.tello_port = 8889
         self.tello_address = (self.tello_ip, self.tello_port)
 
@@ -26,41 +21,31 @@ class drone_status():
         data=data[0].lstrip(':')
         return data
 
-    def battery(self):
-        socket.sendto('command'.encode('utf-8'), self.tello_address) ## Enter SDK Mode   
-        response, ip = socket.recvfrom(1024)
-        data=response.decode()
-        print(data)
-        data = self.data_filter('bat',data)
+    def get_state(self):
+        self.socket.sendto('command'.encode('utf-8'), self.tello_address) ## Enter SDK Mode
+        response = 'ok'
+        try:
+            while response == 'ok' or response == b'ok':
+                outstr = ""
+                response, ip = self.socket.recvfrom(1024)
+                sleep(0.2)
+            outstr = str(response)              
+        except KeyboardInterrupt:
+            pass
+        return outstr
+
+    def print_state(self, state):
+        '''
+        state == bat : battery
+        state == tof : tof sensor
+        '''
+        response = self.get_state()
+        data = self.data_filter(state,response)
         return data
 
-'''    
-if __name__ == "__main__":
-    print(battery())
-    
-    local_ip = ''
-    local_port = 8890 ## read port
-    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
-    socket.bind((local_ip, local_port))
 
-    tello_ip = '192.168.10.1'
-    tello_port = 8889 ## write port
-    tello_address = (tello_ip, tello_port)
-    socket.sendto('command'.encode('utf-8'), tello_address) ## Enter SDK Mode   
-    socket.sendto('battery?'.encode('utf-8'), tello_address) ## Enter SDK Mode   
-    response = socket.recvfrom(1024)
-    outStr = 'Tello Battery:' + str(response.decode)
-    print(outStr)
-    try:
-        index = 0
-        while True:
-            outStr=""
-            response, ip = socket.recvfrom(1024)
-            if response == 'ok':
-                continue
-            outStr = 'Tello State:' + str(response)
-            print(outStr)
-            sleep(0.2)
-    except KeyboardInterrupt:
-        pass
-    '''
+if __name__ == "__main__":
+    status = drone_status(drone_ip = '192.168.137.198', host_ip = '192.168.137.196')
+    while True:
+        print(status.print_state("tof"))
+        sleep(0.2)
