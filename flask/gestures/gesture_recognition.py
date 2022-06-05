69e68e3c22f229f18a5728a7a6d36403c708d9ee
smh0346 <smh0346@naver.com>
2022-06-05 20:55:03 +0900

    Update gesture_recognition.py

--- flask/gestures/gesture_recognition.py
+++ flask/gestures/gesture_recognition.py
@@ -140,10 +140,10 @@ class GestureRecognition:
         return debug_image, gesture_id
 
     def draw_point_history(self, image, point_history):
-        for index, point in enumerate(point_history):
-            if point[0] != 0 and point[1] != 0:
-                cv.circle(image, (point[0], point[1]), 1 + int(index / 2),
-                          (152, 251, 152), 2)
+        #for index, point in enumerate(point_history):
+            #if point[0] != 0 and point[1] != 0:
+                #cv.circle(image, (point[0], point[1]), 1 + int(index / 2),
+                          #(152, 251, 152), 2)
 
         return image
 
